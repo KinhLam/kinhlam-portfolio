28cffc80d8cffc49308f5ed10b05a83cd63baf3e
Lâm Đức Kính <89448648+KinhLam@users.noreply.github.com>
2024-08-23 00:42:06 +0700

    Code tiếp phần còn lại

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,3 +1,14 @@
+"use client";
+
+
+const services = [
+  {
+    num: '01',
+    title: 'Web Development',
+    description: 'Lorem'
+  }
+];
+
 const Services = () => {
   return <div>services page</div>;
 };
